17e27d4fd492d58ba5e86f5121becc95013868c1
ivanmiroshin <ivanmiroshin@gmail.com>
2018-11-02 14:53:25 +0300

    v. 3.0.2

--- src/Adapters/Laravel/SimConfig.php
+++ src/Adapters/Laravel/SimConfig.php
@@ -5,7 +5,6 @@ $baseDir = dirname(dirname(__FILE__));
 return [
     'RootURL' => '/templates/',
     'RootPath' => implode(DIRECTORY_SEPARATOR, [$baseDir,'public','templates']),
-    'Cache' => true,
     'CachePath' => implode(DIRECTORY_SEPARATOR, [$baseDir,'storage','sim','cache']),
     'Debug' => false
 ];
